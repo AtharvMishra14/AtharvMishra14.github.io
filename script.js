ecea93c7b674c41469bab56286a1b02f3a535ad7
Atharv Mishra <atharvmishra14022005@gmail.com>
2026-06-15 23:37:42 +0530

    Portfolio website

---
+++ script.js
@@ -0,0 +1,41 @@
+new Typed("#typing", {
+strings: [
+"Cloud Engineer",
+"AI Developer",
+"Full Stack Developer",
+"AWS Enthusiast"
+],
+typeSpeed: 70,
+backSpeed: 40,
+loop: true
+});
+
+particlesJS("particles-js", {
+particles: {
+number: {
+value: 80
+},
+color: {
+value: "#7c3aed"
+},
+shape: {
+type: "circle"
+},
+opacity: {
+value: 0.4
+},
+size: {
+value: 3
+},
+line_linked: {
+enable: true,
+distance: 150,
+color: "#2563eb",
+opacity: 0.3
+},
+move: {
+enable: true,
+speed: 2
+}
+}
+});
